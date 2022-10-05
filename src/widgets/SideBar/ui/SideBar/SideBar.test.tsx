db384a9ea2e08f43805e690315a378f2e75d564e
Vasya <vpetruin@inbox.ru>
2022-10-05 21:22:28 +0300

    fix unit test for Sidebar, add ComponentRender, add i18nForTests

--- src/widgets/SideBar/ui/SideBar/SideBar.test.tsx
+++ src/widgets/SideBar/ui/SideBar/SideBar.test.tsx
@@ -1,19 +1,20 @@
 import { render, screen, fireEvent } from '@testing-library/react';
+import { ComponentRender } from 'shared/config/tests/componentRender';
 import { SideBar } from 'widgets/SideBar/ui/SideBar/SideBar';
 
 describe('Tests for Sidebar', () => {
     test('Sidebar render test', () => {
-        render(<SideBar />);
+        ComponentRender(<SideBar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
     test('Is sidebar open test', () => {
-        render(<SideBar />);
+        ComponentRender(<SideBar />);
         const toggleBtn = screen.getByTestId('sidebar-btn');
         fireEvent.click(toggleBtn);
         expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
     });
     test('Is sidebar open test', () => {
-        render(<SideBar />);
+        ComponentRender(<SideBar />);
         const toggleBtn = screen.getByTestId('sidebar-btn');
         fireEvent.click(toggleBtn);
         expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
