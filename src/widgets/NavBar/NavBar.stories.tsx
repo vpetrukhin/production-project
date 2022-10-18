d408c4e41cd813033a9df0a651c716c6366f934d
Vasiliy Petrukhin <vpetruin@inbox.ru>
2022-10-18 15:39:59 +0300

    Add story for Auth NavBar

--- src/widgets/NavBar/NavBar.stories.tsx
+++ src/widgets/NavBar/NavBar.stories.tsx
@@ -3,6 +3,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { NavBar } from './NavBar';
 import { ThemeDecorator } from 'shared/config/storybook/decorators/ThemeDecorator';
 import { Theme } from 'app/providers/theme';
+import { StoreDecorator } from 'shared/config/storybook/decorators';
 
 export default {
     title: 'widgets/NavBar',
@@ -16,6 +17,43 @@ const Template: ComponentStory<typeof NavBar> = (args) => <NavBar {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {};
+Primary.decorators = [StoreDecorator({
+    user: {}
+})];
+
 export const PrimaryDark = Template.bind({});
-PrimaryDark.decorators = [ThemeDecorator(Theme.DARK)];
+PrimaryDark.decorators = [
+    ThemeDecorator(Theme.DARK),
+    StoreDecorator({
+        user: {}
+    })
+];
 PrimaryDark.args = {};
+
+
+export const Auth = Template.bind({});
+Auth.args = {};
+Auth.decorators = [
+    StoreDecorator({
+        user: {
+            userInfo: {
+                id: '1',
+                username: 'admin',
+            }
+        }
+    })
+];
+
+export const AuthDark = Template.bind({});
+AuthDark.decorators = [
+    ThemeDecorator(Theme.DARK),
+    StoreDecorator({
+        user: {
+            userInfo: {
+                id: '1',
+                username: 'admin',
+            }
+        }
+    })
+];
+AuthDark.args = {};
