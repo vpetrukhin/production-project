32542b4241839516b2a0f4085b51c003b239e0df
Vasiliy Petrukhin <vpetruin@inbox.ru>
2022-10-25 20:11:08 +0300

    add Story for ProfileCard

---
+++ src/entity/Profile/ui/ProfileCard.stories.tsx
@@ -0,0 +1,52 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { ProfileCard } from './ProfileCard';
+import { ThemeDecorator } from 'shared/config/storybook/decorators/ThemeDecorator';
+import { Theme } from 'app/providers/theme';
+
+export default {
+    title: 'entity/ProfileCard',
+    component: ProfileCard,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    }
+} as ComponentMeta<typeof ProfileCard>;
+
+const Template: ComponentStory<typeof ProfileCard> = (args) => <ProfileCard {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+    data: {
+        age: 22,
+        city: 'Moscow',
+        first: 'name',
+        lastname: 'lastname',
+        username: 'admin',
+        avatar: 'https://images.unsplash.com/photo-1665856314098-4aa9ff7a3d76?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
+    },
+    readonly: true,
+};
+
+export const DefaultDark = Template.bind({});
+DefaultDark.decorators = [ThemeDecorator(Theme.DARK)];
+DefaultDark.args = {
+    data: {
+        age: 22,
+        city: 'Moscow',
+        first: 'name',
+        lastname: 'lastname',
+        username: 'admin',
+        avatar: 'https://images.unsplash.com/photo-1665856314098-4aa9ff7a3d76?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
+    },
+    readonly: true,
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+    isLoading: true,
+};
+
+export const Error = Template.bind({});
+Error.args = {
+    error: 'error',
+};
