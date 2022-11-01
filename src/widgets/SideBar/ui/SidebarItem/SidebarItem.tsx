4733d271ed8eab084b855f2b576bba0b3fb3add8
Vasiliy Petrukhin <vpetruin@inbox.ru>
2022-11-01 13:01:24 +0300

    fix colors

--- src/widgets/SideBar/ui/SidebarItem/SidebarItem.tsx
+++ src/widgets/SideBar/ui/SidebarItem/SidebarItem.tsx
@@ -1,7 +1,7 @@
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink } from 'shared/ui/AppLink/AppLink';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { SidebarItemType } from '../../model/items';
 import cls from './SidebarItem.module.scss';
 
@@ -16,7 +16,7 @@ export const SidebarItem = memo((props: SidebarItemProps) => {
     const { Icon } = item;
 
     return (
-        <AppLink to={item.path} className={classNames(cls.item, {[cls.collapsed]: collapsed})}>
+        <AppLink theme={AppLinkTheme.SECONDARY} to={item.path} className={classNames(cls.item, {[cls.collapsed]: collapsed})}>
             <Icon />
             <span className={cls.link}>{t(item.text)}</span>
         </AppLink>
