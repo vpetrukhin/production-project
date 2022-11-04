badac490494e44ab929d507fff2d1b9d337a0129
Vasiliy Petrukhin <vpetruin@inbox.ru>
2022-11-04 20:50:38 +0300

    add Skeleton

---
+++ src/shared/ui/Skeleton/Skeleton.stories.tsx
@@ -0,0 +1,40 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Sceleton as Skeleton } from './Skeleton';
+import { ThemeDecorator } from 'shared/config/storybook/decorators/ThemeDecorator';
+import { Theme } from 'app/providers/theme';
+
+export default {
+    title: 'shared/Skeleton',
+    component: Skeleton,
+} as ComponentMeta<typeof Skeleton>;
+
+const Template: ComponentStory<typeof Skeleton> = (args) => <Skeleton {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+    width: 150,
+    height: 150,
+};
+
+export const DefaultDark = Template.bind({});
+DefaultDark.decorators = [ThemeDecorator(Theme.DARK)];
+DefaultDark.args = {
+    width: 150,
+    height: 150,
+};
+
+export const Circle = Template.bind({});
+Circle.args = {
+    width: 150,
+    height: 150,
+    border: '50%'
+};
+
+export const CircleDark = Template.bind({});
+CircleDark.decorators = [ThemeDecorator(Theme.DARK)];
+CircleDark.args = {
+    width: 150,
+    height: 150,
+    border: '50%'
+};
