852fcfb93d3329f2a8a7a7abb950d6b5dd8c12ec
Vasiliy Petrukhin <vpetruin@inbox.ru>
2022-11-11 16:34:06 +0300

    add tests for fetchProfileData

---
+++ src/entity/Profile/model/services/fetchProfileData/fetchProfileData.test.ts
@@ -0,0 +1,41 @@
+import { Country } from 'entity/Country';
+import { Currency } from 'entity/Currency';
+import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
+import { Profile } from '../../types/ProfileSchema';
+import { fetchProfileData } from './fetchProfileData';
+
+const mockProfileData: Profile = {
+    id: '1',
+    first: 'Вася',
+    lastname: 'Админович',
+    age: 28,
+    currency: Currency.USD,
+    country: Country.Russia,
+    city: 'Москва',
+    username: 'admin',
+    avatar: 'https://images.unsplash.com/photo-1665856314098-4aa9ff7a3d76?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
+};
+
+describe('tests for fetchProfileData', () => {
+    test('success', async () => {
+        const thunk = new TestAsyncThunk(fetchProfileData);
+
+        thunk.api.get.mockReturnValue(Promise.resolve({ data: mockProfileData }));
+
+        const result = await thunk.callThunk('1');
+
+        expect(thunk.api.get).toHaveBeenCalled();
+        expect(result.meta.requestStatus).toBe('fulfilled');
+        expect(result.payload).toEqual(mockProfileData);
+    });
+    test('error', async () => {
+        const thunk = new TestAsyncThunk(fetchProfileData);
+
+        thunk.api.get.mockReturnValue(Promise.resolve({ status: 403 }));
+
+        const result = await thunk.callThunk('1');
+
+        expect(thunk.api.get).toHaveBeenCalled();
+        expect(result.meta.requestStatus).toBe('rejected');
+    });
+});
