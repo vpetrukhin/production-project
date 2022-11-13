1165891e23c55e5f188c64f3f8438b12613baecd
Vasiliy Petrukhin <vpetruin@inbox.ru>
2022-11-14 00:28:14 +0300

    add tests for User entity

---
+++ src/entity/User/model/selectors/getUserInfo/getUserInfo.test.ts
@@ -0,0 +1,27 @@
+import { StateSchema } from 'app/providers/Redux';
+import { getUserInfo } from './getUserInfo';
+
+describe('tests for getUserInfo.test', () => {
+    test('default', () => {
+        const state: DeepPartial<StateSchema> = {
+            user: {
+                userInfo: {
+                    id: '1',
+                    username: 'admin'
+                }
+            }
+        };
+
+        expect(getUserInfo(state as StateSchema)).toEqual({
+            id: '1',
+            username: 'admin'
+        });
+    });
+    test('with empty state', () => {
+        const state: DeepPartial<StateSchema> = {
+            user: {}
+        };
+
+        expect(getUserInfo(state as StateSchema)).toBe(undefined);
+    });
+});
---
+++ src/entity/User/model/slices/UserSlice/UserSlice.test.ts
@@ -0,0 +1,65 @@
+import { UserSchema } from '../../types/UserSchema';
+import { UserActions, UserReducer } from './UserSlice';
+
+const initialState: UserSchema = {
+    isAuth: false,
+    _inited: false,
+};
+
+describe('tests for UserSlice.test', () => {
+    test('test setUser action', () => {
+        expect(UserReducer(
+            initialState,
+            UserActions.setUser({
+                id: '1',
+                username: 'admin',
+                avatar: '',
+            }),
+        )).toEqual({
+            isAuth: true,
+            _inited: false,
+            userInfo: {
+                id: '1',
+                username: 'admin',
+                avatar: '',
+            }
+        });
+    });
+    test('test initAuth action', () => {
+        expect(UserReducer(
+            {
+                ...initialState,
+                userInfo: {
+                    id: '1',
+                    username: 'admin',
+                    avatar: '',
+                }
+            },
+            UserActions.initAuth(),
+        )).toEqual({
+            isAuth: true,
+            _inited: true,
+            userInfo: {
+                id: '1',
+                username: 'admin',
+                avatar: '',
+            }
+        });
+    });
+    test('test logout action', () => {
+        expect(UserReducer(
+            {
+                ...initialState,
+                userInfo: {
+                    id: '1',
+                    username: 'admin',
+                    avatar: '',
+                }
+            },
+            UserActions.logout(),
+        )).toEqual({
+            isAuth: false,
+            _inited: false,
+        });
+    });
+});
