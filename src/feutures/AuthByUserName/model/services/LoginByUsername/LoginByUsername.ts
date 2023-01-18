378c98b40cce4e5d0a7d2147fb84e97574d4b0ad
Vasiliy Petrukhin <vpetruin@inbox.ru>
2023-01-18 14:05:17 +0300

    fix login

--- src/feutures/AuthByUserName/model/services/LoginByUsername/LoginByUsername.ts
+++ src/feutures/AuthByUserName/model/services/LoginByUsername/LoginByUsername.ts
@@ -13,7 +13,7 @@ export const loginByUsername = createAsyncThunk<User, AuthData, ThunkConfig<stri
         const { dispatch, rejectWithValue, extra } = thunkAPI;
 
         try {
-            const response = await extra.api.post<User>('http://localhost:8000/login', authData);
+            const response = await extra.api.post<User>('/login', authData);
 
             if (!response.data) {
                 throw new Error();
